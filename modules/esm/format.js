426ebbb741809a1dcbc7bcb3afcbd00fb101b675
Evan Welsh <contact@evanwelsh.com>
2021-01-02 10:30:35 -0800

    Cleanup eslint issues.

--- modules/esm/format.js
+++ modules/esm/format.js
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT OR LGPL-2.0-or-later
 // SPDX-FileCopyrightText: 2020 Evan Welsh <contact@evanwelsh.com>
 
-const { vprintf } = imports._format;
+const {vprintf} = imports._format;
 
 /**
  * @param {string} str the string to format (e.g. '%s is blue')
